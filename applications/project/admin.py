4801dc06bf6a44b78dd75410c764f0b47c36150d
g1ftmoon <orozakunov2222@gmail.com>
2022-03-15 22:18:06 +0600

    phone store

---
+++ applications/project/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from applications.project.models import ProductImage, Category, Product
+
+
+class InlineProductImage(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    fields = ('image', )
+
+
+class ProductAdminDisplay(admin.ModelAdmin):
+    inlines = [InlineProductImage, ]
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdminDisplay)
