7571154b9e6bae78ff8f80a5ea9ac8840822ddab
g1ftmoon <orozakunov2222@gmail.com>
2022-03-16 20:12:16 +0600

    Update

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from applications.account.forms import RegistrationForm
+from applications.account.models import User
+
+
+class RegistrationView(CreateView):
+    model = User
+    form_class = RegistrationForm
+    template_name = 'register_page.html'
+    success_url = reverse_lazy('home-page')
+
--- onlineS_project/urls.py
+++ onlineS_project/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from onlineS_project import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('applications.project.urls'))
+    path('', include('applications.project.urls')),
+    path('account/', include('applications.account.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
